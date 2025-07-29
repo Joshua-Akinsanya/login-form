84f83c6a15319d1e3ca4955b06785aa9a1851dcc
Joshua <ojdaraakinsanya@gmail.com>
2025-07-29 11:29:15 +0100

    basic form validation html

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
+// Alert when window too small
+
 window.addEventListener("resize", () =>{
     if(window.innerHeight < 150 || window.innerWidth < 190) {
         alert('Increase your window size, bro !');
     }
 });
+
+
+/*************************************************************************
+ * 
+ * Form Validaton
+ * 
+ *************************************************************************/
