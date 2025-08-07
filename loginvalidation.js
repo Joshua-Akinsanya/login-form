2892e1a1c121060b5647a268ad7903dfcdb4797b
Joshua <ojdaraakinsanya@gmail.com>
2025-08-07 12:48:44 +0100

    Disable Button based on validation

--- loginvalidation.js
+++ loginvalidation.js
@@ -1,58 +1,69 @@
-const login_form = document.querySelector("form[name='login']");
+const loginForm = document.querySelector("form[name='login']");
 
-const email_input = document.querySelector("#email-input");
-const password_input = document.querySelector("#password-input");
+const emailInput = document.querySelector("#email-input");
+const passwordInput = document.querySelector("#password-input");
+const submitButton = document.querySelector("input[type='submit']");
+
 const INVALID_FIELD_CSS_STYLE = 'invalid';
+const DISABLED_BUTTON_STYLE = 'disabled-btn';
 
-const email_error_message = document.querySelector("#email-error-message");
-const password_error_message = document.querySelector("#password-error-message");
+const emailErrorMessage = document.querySelector("#email-error-message");
+const passwordErrorMessage = document.querySelector("#password-error-message");
 
-let is_email_valid = null;
-let is_password_valid = null;
+let is_email_valid = [false, ""];
+let is_password_valid = [false, ""];
 
-login_form.addEventListener('submit', function (event) {
+loginForm.addEventListener('submit', function (event) {
     event.preventDefault();
 
-    is_email_valid = validate_email(email_input.value);
-    is_password_valid = validate_password(password_input.value);
+    is_email_valid = validate_email(emailInput.value);
+    is_password_valid = validate_password(passwordInput.value);
 
     if ( !is_email_valid[0] ) {
-        email_error_message.innerHTML = is_email_valid[1];
+        emailErrorMessage.innerHTML = is_email_valid[1];
     }
     else if ( !is_password_valid[0] ) {
-        password_error_message.innerHTML = is_password_valid[1];
+        passwordErrorMessage.innerHTML = is_password_valid[1];
     }
     else {
         alert('Submitted successfully');
-        login_form.submit();
+        loginForm.submit();
+    }
+});
+
+loginForm.addEventListener('change', function () {
+    if(is_email_valid[0] && is_password_valid[0]) {
+        enable_submit_btn();
     }
 });
 
 
-email_input.addEventListener("change", function () {
-    is_email_valid = validate_email(email_input.value);
+emailInput.addEventListener("change", function () {
+    is_email_valid = validate_email(emailInput.value);
 
     if (is_email_valid[0]) {
-        email_input.classList.remove(INVALID_FIELD_CSS_STYLE);
+        emailInput.classList.remove(INVALID_FIELD_CSS_STYLE);
     } else {
-        if( !email_input.classList.contains(INVALID_FIELD_CSS_STYLE)) {
-            email_input.classList.add(INVALID_FIELD_CSS_STYLE);
+        disable_submit_btn();
+        if( !emailInput.classList.contains(INVALID_FIELD_CSS_STYLE)) {
+            emailInput.classList.add(INVALID_FIELD_CSS_STYLE);
         }
     }
-    email_error_message.innerHTML = is_email_valid[1];
+    emailErrorMessage.innerHTML = is_email_valid[1];
 });
 
-password_input.addEventListener("change", function () {
-    is_password_valid = validate_password(password_input.value);
+passwordInput.addEventListener("change", function () {
+    is_password_valid = validate_password(passwordInput.value);
 
     if (is_password_valid[0]) {
-        password_input.classList.remove(INVALID_FIELD_CSS_STYLE);
+        passwordInput.classList.remove(INVALID_FIELD_CSS_STYLE);
     } else {
-        if( !password_input.classList.contains(INVALID_FIELD_CSS_STYLE)) {
-            password_input.classList.add(INVALID_FIELD_CSS_STYLE);
+        disable_submit_btn();
+        if( !passwordInput.classList.contains(INVALID_FIELD_CSS_STYLE)) {
+            passwordInput.classList.add(INVALID_FIELD_CSS_STYLE);
         }
     }
-    password_error_message.innerHTML = is_password_valid[1];
+    passwordErrorMessage.innerHTML = is_password_valid[1];
 });
 
 function validate_email(email) {
@@ -63,4 +74,16 @@ function validate_email(email) {
 function validate_password(pwd) {
     if(pwd.length < 8) return [false, "Password should be at least 8 characters"];
     else return [true,''];
+}
+
+function disable_submit_btn() {
+    submitButton.disabled = true;
+    if(!submitButton.classList.contains(DISABLED_BUTTON_STYLE) ){
+        submitButton.classList.add(DISABLED_BUTTON_STYLE);
+    }
+}
+
+function enable_submit_btn() {
+    submitButton.disabled = false;
+    submitButton.classList.remove(DISABLED_BUTTON_STYLE);
 }
--- signupvalidation.js
+++ signupvalidation.js
@@ -1,72 +1,83 @@
-const signup_form = document.querySelector("form[name='signup']");
+const signupForm = document.querySelector("form[name='signup']");
 
-const name_input = document.querySelector("#name-input");
-const date_input = document.querySelector("#date-input");
-const email_input = document.querySelector("#email-input");
-const password_input = document.querySelector("#password-input");
+const nameInput = document.querySelector("#name-input");
+const dateInput = document.querySelector("#date-input");
+const emailInput = document.querySelector("#email-input");
+const passwordInput = document.querySelector("#password-input");
+const submitButton = document.querySelector("input[type='submit']");
 
-const name_error_message = document.querySelector("#name-error-message");
-const date_error_message = document.querySelector("#date-error-message");
-const email_error_message = document.querySelector("#email-error-message");
-const password_error_message = document.querySelector("#password-error-message");
+const nameErrorMessage = document.querySelector("#name-error-message");
+const dateErrorMessage = document.querySelector("#date-error-message");
+const emailErrorMessage = document.querySelector("#email-error-message");
+const passwordErrorMessage = document.querySelector("#password-error-message");
 
 const INVALID_FIELD_CSS_STYLE = 'invalid';
+const DISABLED_BUTTON_STYLE = 'disabled-btn';
 
-let is_name_valid = null;
-let is_date_valid = null;
-let is_email_valid = null;
-let is_password_valid = null;
+let is_name_valid = [false, ""];
+let is_date_valid = [true, ""];
+let is_email_valid = [false, ""];
+let is_password_valid = [false, ""];
 
-signup_form.addEventListener("submit", function (event) {
+signupForm.addEventListener("submit", function (event) {
     event.preventDefault();
 
-    is_name_valid = validate_name(name_input.value);
-    is_email_valid = validate_email(email_input.value);
-    is_password_valid = validate_password(password_input.value);
+    is_name_valid = validate_name(nameInput.value);
+    is_email_valid = validate_email(emailInput.value);
+    is_password_valid = validate_password(passwordInput.value);
 
     if (is_name_valid[0] && is_email_valid[0] && is_password_valid[0]) {
         alert('Submitted Successfully!');
-        signup_form.submit();
+        signupForm.submit();
     }
 });
 
-name_input.addEventListener("change", function (event) {
-    is_name_valid = validate_name(name_input.value);
+signupForm.addEventListener("change", function (){
+    if(is_name_valid[0] && is_date_valid[0] && is_email_valid[0] && is_password_valid[0]){
+        enable_submit_btn();
+    }
+});
+
+nameInput.addEventListener("change", function () {
+    is_name_valid = validate_name(nameInput.value);
 
     if(is_name_valid[0]) {
-        name_input.classList.remove(INVALID_FIELD_CSS_STYLE);
+        nameInput.classList.remove(INVALID_FIELD_CSS_STYLE);
     } else {
-        if( !name_input.classList.contains(INVALID_FIELD_CSS_STYLE) ) {
-            name_input.classList.add(INVALID_FIELD_CSS_STYLE);
+        disable_submit_btn();
+        if( !nameInput.classList.contains(INVALID_FIELD_CSS_STYLE) ) {
+            nameInput.classList.add(INVALID_FIELD_CSS_STYLE);
         }
     }
-    name_error_message.innerHTML = is_name_valid[1];
+    nameErrorMessage.innerHTML = is_name_valid[1];
 });
 
-email_input.addEventListener("change", function () {
-    is_email_valid = validate_email(email_input.value);
+emailInput.addEventListener("change", function () {
+    is_email_valid = validate_email(emailInput.value);
 
     if (is_email_valid[0]) {
-        email_input.classList.remove(INVALID_FIELD_CSS_STYLE);
+        emailInput.classList.remove(INVALID_FIELD_CSS_STYLE);
     } else {
-        if( !email_input.classList.contains(INVALID_FIELD_CSS_STYLE)) {
-            email_input.classList.add(INVALID_FIELD_CSS_STYLE);
+        disable_submit_btn();
+        if( !emailInput.classList.contains(INVALID_FIELD_CSS_STYLE)) {
+            emailInput.classList.add(INVALID_FIELD_CSS_STYLE);
         }
     }
-    email_error_message.innerHTML = is_email_valid[1];
+    emailErrorMessage.innerHTML = is_email_valid[1];
 });
 
-password_input.addEventListener("change", function () {
-    is_password_valid = validate_password(password_input.value);
+passwordInput.addEventListener("change", function () {
+    is_password_valid = validate_password(passwordInput.value);
 
     if (is_password_valid[0]) {
-        password_input.classList.remove(INVALID_FIELD_CSS_STYLE);
+        passwordInput.classList.remove(INVALID_FIELD_CSS_STYLE);
     } else {
-        if( !password_input.classList.contains(INVALID_FIELD_CSS_STYLE)) {
-            password_input.classList.add(INVALID_FIELD_CSS_STYLE);
+        disable_submit_btn();
+        if( !passwordInput.classList.contains(INVALID_FIELD_CSS_STYLE)) {
+            passwordInput.classList.add(INVALID_FIELD_CSS_STYLE);
         }
     }
-    password_error_message.innerHTML = is_password_valid[1];
+    passwordErrorMessage.innerHTML = is_password_valid[1];
 });
 
 function validate_name(name) {
@@ -87,4 +98,16 @@ function validate_password(pwd) {
     console.log("Length: "+ pwd.length);
     if(pwd.length < 8) return [false, "Password should be at least 8 characters"];
     else return [true,''];
+}
+
+function disable_submit_btn() {
+    submitButton.disabled = true;
+    if(!submitButton.classList.contains(DISABLED_BUTTON_STYLE) ){
+        submitButton.classList.add(DISABLED_BUTTON_STYLE);
+    }
+}
+
+function enable_submit_btn() {
+    submitButton.disabled = false;
+    submitButton.classList.remove(DISABLED_BUTTON_STYLE);
 }
